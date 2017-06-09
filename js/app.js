043652178573409612bbf158d6d27e9dd54cd8ad
Torian <toriancrane@gmail.com>
2017-06-09 13:56:07 -0700

    Pushing project updates

--- js/app.js
+++ js/app.js
@@ -4,79 +4,101 @@ $(document).ready(function() {
   });
 });
 
-var map;
-      // Create a new blank array for all the listing markers.
-      var markers = [];
-      function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map($('#map')[0], {
-          center: {lat:33.609525, lng: -112.161325},
-          zoom: 14
-        });
-        // These are the real estate listings that will be shown to the user.
-        // Normally we'd have these in a database instead.
-        var locations = [
-        	{title: 'OHYA Sushi, Korean Kitchen & Bar', location: {lat: 33.611548, lng: -112.165117}},
-        	{title: "Pullano's Pizza", location: {lat: 33.612199, lng: -112.169104}},
-        	{title: 'Bangkok Thai B-B-Q', location: {lat: 33.611598, lng: -112.170436}},
-        	{title: 'Rocket Burger', location: {lat: 33.595876, lng: -112.134478}},
-        	{title: "Don Ruben's Mexican", location: {lat: 33.595647, lng: -112.152177}}
+var resLocations = [
+        	{title: 'OHYA Sushi, Korean Kitchen & Bar', address: '4920 W Thunderbird Rd, Glendale, AZ 85306', location: {lat: 33.611548, lng: -112.165117}},
+        	{title: "Pullano's Pizza", address: '13848 N 51st Ave, Glendale, AZ 85306', location: {lat: 33.612199, lng: -112.169104}},
+        	{title: 'Bangkok Thai B-B-Q', address: '13828 N 51st Ave, Glendale, AZ 85306', location: {lat: 33.611598, lng: -112.170436}},
+        	{title: 'Rocket Burger', address: '12038 N 35th Ave #2, Phoenix, AZ 85029', location: {lat: 33.595876, lng: -112.134478}},
+        	{title: "Don Ruben's Mexican", address: '4323 W Cactus Rd, Glendale, AZ 85304', location: {lat: 33.595647, lng: -112.152177}}
         ];
-        var largeInfowindow = new google.maps.InfoWindow();
-        var bounds = new google.maps.LatLngBounds();
-        // The following group uses the location array to create an array of markers on initialize.
-        for (var i = 0; i < locations.length; i++) {
-          // Get the position from the location array.
-          var position = locations[i].location;
-          var title = locations[i].title;
-          // Create a marker per location, and put into markers array.
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: i
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          // Create an onclick event to open an infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-        }
-        $('#show-listings').on('click', showListings);
-        $('#hide-listings').on('click', hideListings);
-      }
-      // This function populates the infowindow when the marker is clicked. We'll only allow
-      // one infowindow which will open at the marker that is clicked, and populate based
-      // on that markers position.
-      function populateInfoWindow(marker, infowindow) {
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-          infowindow.marker = marker;
-          infowindow.setContent('<div>' + marker.title + '</div>');
-          infowindow.open(map, marker);
-          // Make sure the marker property is cleared if the infowindow is closed.
-          infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-          });
-        }
-      }
 
-      // This function will loop through the markers array and display them all.
-      function showListings() {
-        var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(map);
-          bounds.extend(markers[i].position);
-        }
-        map.fitBounds(bounds);
-      }
-      
-      // This function will loop through the listings and hide them all.
-      function hideListings() {
-        for (var i = 0; i < markers.length; i++) {
-          markers[i].setMap(null);
-        }
-      }
+var map;
+var markers = [];
+
+// Constructor creates a new map - only center and zoom are required.
+function initMap() {
+	map = new google.maps.Map($('#map')[0], {
+	center: {lat:33.609525, lng: -112.161325},
+	zoom: 14
+	});
+	
+}
+
+// My ViewModel
+var ViewModel = function(){
+	var self = this;
+	this.searchFilter = ko.observable('');
+	this.locations = ko.observableArray(resLocations);
+
+	// Creates infowindow object to display restaurant info
+    var largeInfoWindow = new google.maps.InfoWindow();
+
+    // Limits the map to display all the locations on the screen
+    var bounds = new google.maps.LatLngBounds();
+
+    // Filters the list of restaurants
+    this.visiblePlaces = ko.computed(function(){
+       return this.locations().filter(function(location){
+           if(!self.filter() || location.title.toLowerCase().indexOf(self.filter().toLowerCase()) !== -1)
+             return location;
+       });
+   },this);
+}
+        
+ko.applyBindings(new ViewModel());
+
+// The following group uses the location array to create an array of markers on initialize.
+for (var i = 0; i < locations.length; i++) {
+	// Get the position from the location array.
+	var position = locations[i].location;
+	var title = locations[i].title;
+	// Create a marker per location, and put into markers array.
+	var marker = new google.maps.Marker({
+	map: map,
+	position: position,
+	title: title,
+	animation: google.maps.Animation.DROP,
+	id: i
+	});
+	// Push the marker to our array of markers.
+	markers.push(marker);
+	// Create an onclick event to open an infowindow at each marker.
+	marker.addListener('click', function() {
+	populateInfoWindow(this, largeInfowindow);
+	});
+}
+$('#show-listings').on('click', showListings);
+$('#hide-listings').on('click', hideListings);
+
+// This function populates the infowindow when the marker is clicked.
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infowindow.addListener('closeclick',function(){
+		infowindow.setMarker = null;
+	});
+}
+}
+
+// This function will loop through the markers array and display them all.
+function showListings() {
+var bounds = new google.maps.LatLngBounds();
+// Extend the boundaries of the map for each marker and display the marker
+for (var i = 0; i < markers.length; i++) {
+  markers[i].setMap(map);
+  bounds.extend(markers[i].position);
+}
+map.fitBounds(bounds);
+}
+
+// This function will loop through the listings and hide them all.
+function hideListings() {
+for (var i = 0; i < markers.length; i++) {
+  markers[i].setMap(null);
+}
+}
+

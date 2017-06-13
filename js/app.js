05cd40e3374e305b7fba6fc9e59aa983882c3b69
toriancrane <toriancrane@gmail.com>
2017-06-12 19:53:37 -0700

    changed from Yelp to Wikipedia due to multiple unresolved errors

--- js/app.js
+++ js/app.js
@@ -8,14 +8,6 @@ function googleMapError(){
   alert("Google Maps API is not loading...");
 }
 
-// var resLocations = [
-//         	{title: 'OHYA Sushi, Korean Kitchen & Bar', address: '4920 W Thunderbird Rd, Glendale, AZ 85306', location: {lat: 33.611548, lng: -112.165117}},
-//         	{title: "Pullano's Pizza", address: '13848 N 51st Ave, Glendale, AZ 85306', location: {lat: 33.612199, lng: -112.169104}},
-//         	{title: 'Bangkok Thai B-B-Q', address: '13828 N 51st Ave, Glendale, AZ 85306', location: {lat: 33.611598, lng: -112.170436}},
-//         	{title: 'Rocket Burger', address: '12038 N 35th Ave #2, Phoenix, AZ 85029', location: {lat: 33.595876, lng: -112.134478}},
-//         	{title: "Don Ruben's Mexican", address: '4323 W Cactus Rd, Glendale, AZ 85304', location: {lat: 33.595647, lng: -112.152177}}
-//         ];
-
 var map;
 var resMarker;
 var markers = [];
@@ -33,8 +25,6 @@ function initMap() {
 
 	// Limits the map to display all the locations on the screen
     var bounds = new google.maps.LatLngBounds();
-
-
 }
 
 //My list model that calls the info from my json file
@@ -45,6 +35,7 @@ function ResListModel(restaurants){
 		url: '/js/reslocations.json'
 	}).done(function(data){
 		var resLocations = data.resLocations;
+		console.log(resLocations);
 		resLocations.forEach(function(coordinates, i){
 			viewModel.restaurants.push(new LocationModel(coordinates, viewModel));
 		});
@@ -78,101 +69,75 @@ var LocationModel = function(coordinates, viewModel) {
 						'<div>' + self.city + ',' + self.state + self.zipcode + '</div>';
 			});
 			self.resMarker.addListener('click', function(){
-				function removeSpaces(str){
-					str.replace(/\s+/g, '-').toLowerCase();
+				  // Adding Wikipedia API
+			        var wikiUrl = 'https://en.wikipedia.org/w/api.php?'+
+			                      'action=opensearch&search=' + self.title +
+			                      '&format=json&callback=wikiCallback';
+
+                   $.ajax({
+			          url: wikiUrl,
+			          dataType: "jsonp"
+			        }).done(function(response){
+			          var resInfo = response[3][0];
+			          if (resInfo === undefined){
+			          var errorURL = 'Wikipedia Article unavailabe for' + self.title;
+			          viewModel.largeInfoWindow.setContent(self.cStr() + errorURL);
+			          } else {
+			          var successURL = '<div><a href="' + resInfo + '" target="_blank">' + self.title + '</a></div>';
+			          //Set infowindow content
+			          viewModel.largeInfoWindow.setContent(self.cStr() + successURL);}
+			          // Open LargeInfoWindow
+			          viewModel.largeInfoWindow.open(map, self.resMarker);
+			        }).fail(function(){
+			          //Set infowindow content
+			          viewModel.largeInfoWindow.setContent(self.cStr() + "<em><br>Wikipedia data has failed to load.</em>");
+			          // Open LargeInfoWindow
+			          viewModel.largeInfoWindow.open(map, self.resMarker);
+			        });
+			        // Marker Animations
+			        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+			        setTimeout (function(){self.marker.setAnimation(null);}, 750);
+			      });
 				}
-				var yelpURL = 'api.yelp.com/v2/business/' + removedSpaces(self.title)
 			})
-			www.yelp.com/biz/name-city
-		}
-	})
-}
+		};
 
 // My ViewModel
-function ViewModel(){
-	var self = this;
-	this.searchFilter = ko.observable();
-	this.locations = ko.observableArray(resLocations);	
+var ViewModel = function(LocationModel) {
+  var self = this;
+  self.restaurants = ko.observableArray();
+  self.google = ko.observable(!!window.google);
 
-    // Filters the list of restaurants
-    this.visibleLocations = ko.computed(function(){
-       return this.locations().filter(function(location){
-           if(!self.searchFilter() || location.title.toLowerCase().indexOf(self.searchFilter().toLowerCase()) !== -1)
-             return location;
-       });
-   },this);
-}
-        
+  ResListModel(self.restaurants);
 
+  self.infoWindowCreation = ko.computed(function(){
+    if (self.google()){
+      self.largeInfoWindow = new google.maps.InfoWindow();
+    }
+  });
 
+  // Search Filter
+  self.resSearch = ko.observable("");
 
-// Creates infowindow object to display restaurant info
-    var largeInfoWindow = new google.maps.InfoWindow();
-
-    
-
- //    // The following uses the location array to create an array of markers on initialize.
-	// for (var i = 0; i < resLocations.length; i++) {
-	// 	// Get the position from the location array.
-	// 	var position = resLocations[i].location;
-	// 	var title = resLocations[i].title;
-	// 	// Create a marker per location, and put into markers array.
-	// 	var marker = new google.maps.Marker({
-	// 	map: map,
-	// 	position: position,
-	// 	title: title,
-	// 	animation: google.maps.Animation.DROP,
-	// 	id: i
-	// 	});
-	// 	// Push the marker to our array of markers.
-	// 	markers.push(marker);
-	// 	// Create an onclick event to open an infowindow at each marker.
-	// 	marker.addListener('click', function() {
-	// 	populateInfoWindow(this, largeInfowindow);
-	// 	});
-	// }
+  self.visibleLocations = ko.computed(function(){
+    var filter = self.resSearch().toLowerCase();
+    return ko.utils.arrayFilter(self.restaurants(), function(coordinates){
+      var resResults = coordinates.title.toLowerCase().indexOf(filter) >= 0;
+      if(coordinates.resMarker){
+        coordinates.resMarker.setVisible(resResults);
+      }
+      return resResults;
+    });
+  });
 
 
-
-ko.applyBindings(new ViewModel());
-
-
-
-
+  // Click binding for markers
+  self.markerAnimator = function(coordinates) {
+    google.maps.event.trigger(coordinates.resMarker, 'click');
+  };
 
 
-// $('#show-listings').on('click', showListings);
-// $('#hide-listings').on('click', hideListings);
+};
 
-// // This function populates the infowindow when the marker is clicked.
-// function populateInfoWindow(marker, infowindow) {
-// 	// Check to make sure the infowindow is not already opened on this marker.
-// 	if (infowindow.marker != marker) {
-// 		infowindow.marker = marker;
-// 		infowindow.setContent('<div>' + marker.title + '</div>');
-// 		infowindow.open(map, marker);
-// 		// Make sure the marker property is cleared if the infowindow is closed.
-// 		infowindow.addListener('closeclick',function(){
-// 		infowindow.setMarker = null;
-// 	});
-// }
-// }
-
-// // This function will loop through the markers array and display them all.
-// function showListings() {
-// var bounds = new google.maps.LatLngBounds();
-// // Extend the boundaries of the map for each marker and display the marker
-// for (var i = 0; i < markers.length; i++) {
-//   markers[i].setMap(map);
-//   bounds.extend(markers[i].position);
-// }
-// map.fitBounds(bounds);
-// }
-
-// // This function will loop through the listings and hide them all.
-// function hideListings() {
-// for (var i = 0; i < markers.length; i++) {
-//   markers[i].setMap(null);
-// }
-// }
-
+viewModel = new ViewModel();
+ko.applyBindings(viewModel);
